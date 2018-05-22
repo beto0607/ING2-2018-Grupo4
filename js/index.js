a885967060bbf148982c93d28c4d464c2a25fd5d
Roberto Albanesi <beto0607@gmail.com>
2018-05-22 02:13:14 -0300

    Se avanzo con index. Se maqueto login y signup. Se maqueto terminos y condiciones de uso. Se empezo a maquetar el Dashboard del usuario

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-$("#registerLink").on("click",function(){
-	window.scroll(0, $("#registerSection").position().top);
+function registerLinkTap(){
+	window.scroll(0, $("#registerSection").position().top - 100);
+	$('#signTabs a[href="#signin"]').tab('show');
+}
+
+
+
+
+
+$(document).ready(function(){
+
+	$("#registerLink").on("click", registerLinkTap);
+
+	if(window.location.href.indexOf("#registerSection") != -1){
+		registerLinkTap();
+	}
 });
