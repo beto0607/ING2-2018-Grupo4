a3caaaddec3fca4d25808868203821f21f5c5da8
Roberto Albanesi <beto0607@MacBook-Pro-de-Roberto.local>
2018-05-28 14:48:59 -0300

    se modifico aceso a la bd, para terner compatibilidad con localhost y con 0.0.0.0

--- lib/db/database.php
+++ lib/db/database.php
@@ -3,8 +3,9 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=unaventon;charset=utf8', 'root', '');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+      $pdo = new PDO('mysql:host='.$_SERVER['SERVER_NAME']'.;dbname=unaventon;charset=utf8', 'root', '');
+      
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
 }
