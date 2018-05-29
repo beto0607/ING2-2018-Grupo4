dca520c40cd0fef92f53700c1a43bbfcaae24023
Roberto Albanesi <beto0607@gmail.com>
2018-05-29 17:11:18 -0300

    un mensaje

--- front/js/home.js
+++ front/js/home.js
@@ -1,8 +1,10 @@
 var URLs = {
 	travelsList:"../index.php?c=viajes&a=Listar&debug=1",
+	travelAdd:"../index.php?c=viaje&a=Guardar&debug=1",
 
 	vehiclesList:"../index.php?c=vehiculo&a=Listar&debug=1",
 	vehiclesRemove:"../index.php?c=vehiculo&a=Eliminar&debug=1",
+	vehiclesAdd:"../index.php?c=vehiculo&a=Guardar&debug=1",
 	vehiclesModify:"../index.php?c=vehiculo&a=Guardar&debug=1",
 
 	userInfo: "../index.php?c=usuario&a=Obtener&debug=1",
@@ -23,7 +25,12 @@ $(document).ready(function(){
 	/*------------Eventos generales------------*/
 	$("#optionsContainer .option").on("click", function(e){
 		const s = $(this).attr("data-target");
-		setTimeout(function(){window.scrollTo(0, $(s).position().top - 100);},500);
+		if(s == "#vehiclesContainer"){
+			$("#vehiclesContainer .infoButton").on("click",vehicleInfoButtonClicked);
+		}
+		$("div.infoContainer").hide();
+		$(s).show();
+		//setTimeout(function(){window.scrollTo(0, $(s).position().top - 100);},500);
 		return e;
 	});
 	$("#signoutButton").on("click", signoutClick);
@@ -32,11 +39,7 @@ $(document).ready(function(){
 	$("#userInfoModal .editButton").on("click", userInfoInputClick);
 	/*------------Eventos del manejo de viajes------------*/
 	$(".travelListItem").on("click", travelListItemClick);
-	/*------------Eventos del manejo de vehiculos------------*/
-	$("#vehiclesContainer .infoButton").on("click",vehicleInfoButtonClicked);
-	$("#modifyVehicleDialog .editButton").on("click",vehicleModalEdit);
-	$("#addVehicleDialog").on("show.bs.modal",vehicleAddModalOpens);
-	$("#addVehicleDialog button.saveVehicle").on("click", vehicleAddSubmit);
+
 });
 /*------------Funciones del manejo de viajes---------------*/
 function loadLastTravels(){
@@ -131,10 +134,10 @@ function userInfoSave(){
 			hideSpinner();
 			try {
 				d = parseJSON(d);
-				if(d.status == "ok" || d.status == "1"){
+				if(d.success == "ok" || d.success == "1"){
 					reloadPage();
 				}else{
-					bAlert(d.error);
+					bAlert(d.mensaje);
 				}
 			} catch (e) {
 				bAlert("Ocurrió un error.");
@@ -150,9 +153,28 @@ function vehicleModalEdit(e){
 	$("#modifyVehicleDialog input, #modifyVehicleDialog textarea").removeAttr("disabled");
 	$("#modifyVehicleDialog .modal-title").text("Modificar vehículo");
 }
-function vehicleInfoButtonClicked(e){
-	$("#modifyVehicleDialog .editButton").hide();
+function getVehicle(id){
+	for(var i = 0; i<vehicles.length; i++){
+		if(id == vehicles[i].id){
+			return vehicles[i];
+		}
+	}
+	return null;
+}
+
+function vehicleInfoButtonClicked(){
+	var vID = $(this).parent().parent().attr("vehicle-id");
+	$("#modifyVehicleDialog").attr("vehicle-id", vID);
+	var v = getVehicle(vID);
+	var form = $("#modifyVehicleDialog form");
+	console.log(v);
+	setInputValue(form, "modify-vehicle-domain", v.dominio);
+	setInputValue(form, "modify-vehicle-brand", v.marca);
+	$("textarea[name=\"modify-vehicle-desc\"]").val(v.descripcion);
+	setInputValue(form, "modify-vehicle-size", parseInt(v.plazas));
+	setInputValue(form, "modify-vehicle-model", v.modelo);
 	$("#modifyVehicleDialog").modal("show");
+//	$("#modifyVehicleDialog").
 }
 function loadVehicles(){
 	$.post(URLs.vehiclesList, {'idUsuario': userID})
@@ -163,10 +185,14 @@ function loadVehicles(){
 					for(var i = 0; i< d.length; i++){
 						var rendered = Mustache.render(template, d[i]);
 						$("#vehiclesContainer tbody").append(rendered);
+						$("#addTravelVehiclesSelect").append(
+							'<option value="'+d[i].id+'">'+d[i].dominio+' - '+d[i].marca+' - '+d[i].modelo+'</option>'
+						);
 					}
 					infoLoaded("vehiclesList");
 				});
 				vehicles = d;
+				ConfigureVehicles();
 			}catch(e){
 				console.log(e);
 				console.log(d);
@@ -177,20 +203,65 @@ function loadVehicles(){
 function deleteVehicle(){
 	var vID = $("#modifyVehicleDialog").attr("vehicle-id");
 	if(!vID){return;}
-	showSpinner();
-	$.post(URLs.vehiclesRemove, {id: vID})
+	$.post(URLs.vehiclesRemove, {id: vID, idUsuario: userID})
 		.done(function(d,s){
-			location.reload();
+			console.log(d);
+			d = parseJSON(d);
+			if(d.success == "1" || d.success == "ok"){
+				bAlertCallback("Vehículo eliminado correctamente.", reloadPage);
+			}else{
+				bAlertCallback(""+d.mensaje, reloadPage);
+			}
 		})
 		.fail(onFailPost);
 }
+function vehicleModifySubmit(){
+	$("#modifyVehicleForm").submit();
+}
+function vehicleModifyValidateForm(){
+	$("#modifyVehicleForm").validate({
+		onfocusout: false,
+		rules: {
+			"modify-vehicle-domain": {
+				required: true
+			},
+			"modify-vehicle-brand": {
+				required: true
+			},
+			"modify-vehicle-desc": {
+				required: true
+			},
+			"modify-vehicle-model": {
+				required: true
+			},
+			"modify-vehicle-size": {
+				required: true,
+				min:1
+			}
+		},
+		submitHandler:modifyVehicle
+	});
+}
 function modifyVehicle(){
-	var vID = $("#modifyVehicleDialog").attr("vehicle-id");
-	if(!vID){return;}
-	showSpinner();
-	$.post(URLs.vehiclesModify, {id: vID})
+	var form = $("#modifyVehicleDialog form");
+	var data = {
+		dominio: getInputValue(form, "modify-vehicle-domain"),
+		marca: getInputValue(form, "modify-vehicle-brand"),
+		descripcion: $("textarea[name=\"modify-vehicle-desc\"]").val(),
+		plazas: getInputValue(form, "modify-vehicle-size"),
+		modelo: getInputValue(form, "modify-vehicle-model"),
+		id: $("#modifyVehicleDialog").attr("vehicle-id"),
+		idUsuario: userID
+	};
+	$.post(URLs.vehiclesModify, data)
 		.done(function(d,s){
-			location.reload();
+			console.log(d);
+			d = parseJSON(d);
+			if(d.success == "ok" || d.success == "1"){
+				bAlertCallback("Vehículo modificado.", reloadPage);
+			}else{
+				bAlertCallback(d.mensaje, reloadPage);
+			}
 		})
 		.fail(onFailPost);
 }
@@ -231,10 +302,93 @@ function addVehicle(){
 		modelo: getInputValue(form, "add-vehicle-model"),
 		idUsuario: userID
 	};
-	showSpinner();
-	$.post(URLs.vehiclesModify, data)
+	$.post(URLs.vehiclesAdd, data)
 		.done(function(d,s){
-			bAlert("Vehículo eliminado", reloadPage);
+			console.log(d);
+			d = parseJSON(d);
+			if(d.success == "1" || d.success == "ok"){
+				bAlert("Vehículo agregado.", reloadPage);
+			}else{
+				bAlert(d.mensaje, reloadPage);
+			}
+		})
+		.fail(onFailPost);
+}
+/*---------------------Funciones-------------------------------*/
+function addTravelSubmit(){
+	$("#addTravelForm").submit();
+}
+}
+function travelAddValidateForm(){
+
+	$("#addTravelForm").validate({
+		onfocusout: false,
+		rules: {
+			"add-travel-vehicles": {
+				required: true
+			},
+			"add-travel-size": {
+				required: true
+			},
+			"add-travel-origen": {
+				required: true
+			},
+			"add-travel-destino": {
+				required: true
+			},
+			"add-travel-date": {
+				required: true
+			},
+			"add-travel-hour": {
+				required: true
+			},
+			"add-travel-cbu": {
+				required: true
+			},
+			"add-travel-monto": {
+				required: true,
+				min: 1
+			},
+			"add-travel-desc": {
+				required: true
+			},
+			"add-travel-type":{
+				required: true
+			},
+			"add-travel-till-date":{
+				required: true
+			},
+		},
+		submitHandler:addTravel
+	});
+}
+function addTravel(){
+	var form = $("#addTravelForm");
+	var date = getInputValue(form, "add-travel-date").split("-").join("");
+	date += " " + getInputValue(form, "add-travel-hour");
+	var dateTill = getInputValue(form,"add-travel-type") == "O" ?
+		date :
+		getInputValue(form, "add-travel-till-date") + " " + getInputValue(form, "add-travel-hour");
+	var data = {
+		idUsuario: userID,
+		idVehiculo: getInputValue(form, "add-travel-vehicles"),
+		plazas: getInputValue(form, "add-travel-size"),
+		origen: getInputValue(form, "add-travel-origen"),
+		destino: getInputValue(form, "add-travel-destino"),
+		fecha: date,
+		tipoAlta: dateTill,
+		descripcion: $("textarea[name=\"add-travel-desc\"]").val()
+	}
+	console.log(data);
+	$.post(URLs.travelAdd, data)
+		.done(function(d,s){
+			console.log(d);
+			d= parseJSON(d);
+			if(d.success == "1"){
+				bAlertCallback("Viaje creado.", reloadPage);
+			}else{
+				bAlertCallback(""+d.mensaje, reloadPage);
+			}
 		})
 		.fail(onFailPost);
 }
@@ -254,21 +408,52 @@ function infoLoaded(item){
 		hideSpinner();
 	}
 }
+function ConfigureVehicles(){
+	$("#modifyVehicleDialog input, #modifyVehicleDialog textarea").attr("disabled", "true");
+	$("#modifyVehicleDialog input, #modifyVehicleDialog textarea").removeAttr("value");
+	/*------------Eventos del manejo de vehiculos------------*/
+//	$("#vehiclesContainer .infoButton").on("click",vehicleInfoButtonClicked);
+	$("#modifyVehicleDialog .editButton").on("click",vehicleModalEdit);
+	$("#addVehicleDialog").on("show.bs.modal",vehicleAddModalOpens);
+
+	$("#modifyVehicleDialog #buttonSaveVehicle").on("click", vehicleModifySubmit);
+	$("#addVehicleDialog button.saveVehicle").on("click", vehicleAddSubmit);
+	$("#buttonDeleteVehicle").on("click", deleteVehicle);
+}
+function ConfigureTravels(){
+	$("#addTravelVehiclesSelect").on("change",function(){
+		var vID = ($(this).val());
+		var v = getVehicle(vID);
+		$("#addTravelForm input[name=\"add-travel-size\"]").attr("max", v.plazas);
+	});
+	$("#addTravelTypeSelect").on("change",function(){
+		if($(this).val() == "O"){
+			$("#addTravelTillDate").removeAttr("disabled");
+		}else{
+			$("#addTravelTillDate").attr("disabled","true");
+		}
+	});
+	$("#buttonSaveTravel").on("click", addTravelSubmit);
+}
 function Configure(){
 	configureValidatorMessages();
 	$(".fa-spinner").hide();
-	$("#modifyVehicleDialog .fa-spinner").show();
-	$("#modifyVehicleDialog .editButton").hide();
-	$("#modifyVehicleDialog form").hide();
-	$("#modifyVehicleDialog input, #modifyVehicleDialog textarea").attr("disabled", "true");
-	$("#modifyVehicleDialog input, #modifyVehicleDialog textarea").removeAttr("value");
+
 	showSpinner();
 	loadLastTravels();
 	loadVehicles();
 	loadUserInfo();
 
 	vehicleAddValidateForm();
+	vehicleModifyValidateForm();
 	userInfoValidateForm();
+	travelAddValidateForm();
+
+
+	ConfigureTravels();
+}
+function setInputValue(form, input, v){
+	return $(form).find("input[name=\""+input+"\"]").val(v);
 }
 function getInputValue(form, input){
 	return $(form).find("input[name=\""+input+"\"]").val();
